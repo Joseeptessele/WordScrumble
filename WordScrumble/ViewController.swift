2dc07c81a49bd088c0363f0d4a95b591c66f358d
Jose Pedron Tessele <josetesselep@gmail.com>
2019-08-26 17:14:44 -0300

    Initial Commit

---
+++ WordScrumble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordScrumble
+//
+//  Created by José Eduardo Pedron Tessele on 26/08/19.
+//  Copyright © 2019 José P Tessele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
